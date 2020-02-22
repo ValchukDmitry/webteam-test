5216c3c2567b13f5ef959bedd585daaff3d223d4
ValchukDmitry <dmitry.valchuk@7bits.it>
2020-02-22 03:19:03 +0300

    change building from fatjar to zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,7 @@
-buildscript {
-    var kotlin_version: String by extra
-    kotlin_version = "1.3.61"
 
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath(kotlin("gradle-plugin", kotlin_version))
-    }
-}
 
 plugins {
     kotlin("jvm") version "1.3.61"
-    id("com.github.johnrengelman.shadow").version("5.1.0")
 }
 
 group = "com.jetbrains"
@@ -28,9 +17,7 @@ dependencies {
     implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
     implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.228"))
     implementation("com.amazonaws:aws-java-sdk-s3")
-    compile("org.apache.logging.log4j:log4j-core:2.12.1")
-    compile("org.apache.logging.log4j:log4j-api:2.12.1")
-    compile("com.amazonaws:aws-lambda-java-log4j2:1.0.0")
+    implementation("com.amazonaws:aws-lambda-java-log4j2:1.0.0")
 
     testCompile("junit:junit:4.12")
     testCompile("io.mockk:mockk:1.9")
@@ -42,5 +29,17 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    val buildZip by creating(Zip::class) {
+        from(compileKotlin)
+        from(processResources)
+        into("lib") {
+            from(configurations.runtimeClasspath)
+        }
+    }
+
+    build {
+        dependsOn(buildZip)
     }
 }
