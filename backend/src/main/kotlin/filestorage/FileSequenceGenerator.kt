d219dd9ebdbd295d7e387302691a4e0e09cac458
ValchukDmitry <dmitry.valchuk@7bits.it>
2020-02-29 18:13:56 +0300

    add small javadoc about vendor lock-in to FileSequenceGenerator

--- backend/src/main/kotlin/filestorage/FileSequenceGenerator.kt
+++ backend/src/main/kotlin/filestorage/FileSequenceGenerator.kt
@@ -1,5 +1,9 @@
 package filestorage
 
+
+/**
+ * Interface to avoid vendor lock-in with AWS S3
+ */
 interface FileSequenceGenerator {
     fun getFiles(directoryName: String = ""): Sequence<File>
 }
