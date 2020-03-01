62f3489263b35339b8ff647c28de6a2c5a3978dc
ValchukDmitry <dmitry.valchuk@7bits.it>
2020-03-01 23:13:17 +0300

    small fix of path generating in navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -7,14 +7,14 @@ class Navigation extends Component {
 
     render() {
         let { pathname } = this.props.location;
-        pathname = pathname ? pathname : "/";
+        pathname = pathname ? pathname : "";
         pathname = "/" + pathname
         const pathParts = pathname.split("/").filter(a => a)
         let path = ""
         const links = pathParts.map(pathElem => {
             path = `${path}/${pathElem}`
             return (
-                  <Link key={`${path}`} to={`${path}`}>{`/${pathElem}`}</Link>
+                  <Link key={`${path}`} to={`${path}/`}>{`/${pathElem}`}</Link>
             );
         });
         links.unshift(<Link key="/" to="/">root</Link>)
